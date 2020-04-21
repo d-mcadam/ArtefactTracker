e42e4a45618cac65c07abefa13491fb8b1ed5600
dylan mcadam <dylan@dylanmcadam.co.uk>
2020-04-21 14:52:30 +0100

    Update Storage.java
    declared storage class for all data

--- app/src/main/java/com/example/artefacttrackerapp/Data/Storage.java
+++ app/src/main/java/com/example/artefacttrackerapp/Data/Storage.java
@@ -1,4 +1,34 @@
 package com.example.artefacttrackerapp.Data;
 
+import java.util.ArrayList;
+
 public class Storage {
+
+    //<editor-fold defaultstate="collapsed" desc="Game Artefacts>
+    private final ArrayList<GameArtefact> artefacts;
+    public ArrayList<GameArtefact> Artefacts(){ return this.artefacts; }
+    public boolean AddArtefact(GameArtefact artefact){ return this.artefacts.add(artefact); }
+    public boolean DeleteArtefact(GameArtefact artefact){ return this.artefacts.remove(artefact); }
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Collectors>
+    private final ArrayList<Collector> collectors;
+    public ArrayList<Collector> Collectors(){ return this.collectors; }
+    public boolean AddCollector(Collector collector){ return this.collectors.add(collector); }
+    public boolean DeleteCollector(Collector collector){ return this.collectors.remove(collector); }
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Collections>
+    private final ArrayList<Collection> collections;
+    public ArrayList<Collection> Collections(){ return this.collections; }
+    public boolean AddCollection(Collection collection){ return this.collections.add(collection); }
+    public boolean DeleteCollection(Collection collection){ return this.collections.remove(collection); }
+    //</editor-fold>
+
+    public Storage(){
+        this.artefacts = new ArrayList<>();
+        this.collectors = new ArrayList<>();
+        this.collections = new ArrayList<>();
+    }
+
 }
