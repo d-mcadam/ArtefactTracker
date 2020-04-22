3551b4beae650efeeae5ba30ea8f01bf0164d636
dylan mcadam <dylan@dylanmcadam.co.uk>
2020-04-22 21:18:31 +0100

    Update AddArtefactActivity.java
    adding material requirements via dialog inputs

--- app/src/main/java/com/example/artefacttrackerapp/Activities/AddArtefactActivity.java
+++ app/src/main/java/com/example/artefacttrackerapp/Activities/AddArtefactActivity.java
@@ -1,21 +1,28 @@
 package com.example.artefacttrackerapp.Activities;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.artefacttrackerapp.Data.MaterialRequirement;
 import com.example.artefacttrackerapp.R;
 
 import java.util.ArrayList;
+
+import static com.example.artefacttrackerapp.Activities.MainActivity.storage;
 
 public class AddArtefactActivity extends AppCompatActivity {
 
@@ -60,6 +67,71 @@ public class AddArtefactActivity extends AppCompatActivity {
     }
 
     public void AddMaterialRequirement(View v){
+
+        final Context thisContext = this;
+
+        AlertDialog.Builder nameDialog = new AlertDialog.Builder(thisContext);
+        nameDialog.setTitle("Material Name");
+
+        final LayoutInflater inflater = getLayoutInflater();
+
+        View nameDialogView = inflater.inflate(R.layout.dialog_material_name, null);
+
+        ArrayList<String> spinnerValues = new ArrayList<>();
+        for (String m : storage.Materials()){
+            if (!requirementArrayList.stream().anyMatch((i) -> i.title.equals(m))){
+                spinnerValues.add(m);
+            }else{
+                int i = 0;
+            }
+        }
+
+        final Spinner nameDialogSpinner = nameDialogView.findViewById(R.id.spinnerMaterialNames);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(thisContext, R.layout.support_simple_spinner_dropdown_item, spinnerValues);
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        nameDialogSpinner.setAdapter(adapter);
+
+        nameDialog.setView(nameDialogView)
+            .setPositiveButton("Next", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+
+                    final String name = nameDialogSpinner.getSelectedItem().toString();
+
+                    AlertDialog.Builder qtyDialog = new AlertDialog.Builder(thisContext);
+                    qtyDialog.setTitle("Quantity Required");
+
+                    View qtyDialogView = inflater.inflate(R.layout.dialog_material_quantity, null);
+                    final EditText qtyDialogField = qtyDialogView.findViewById(R.id.editTextMaterialReqQuantity);
+
+                    qtyDialog.setView(qtyDialogView)
+                        .setPositiveButton("Save", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                                int quantity = Integer.parseInt(qtyDialogField.getText().toString().trim());
+
+                                MaterialRequirement materialRequirement = new MaterialRequirement(name, quantity);
+
+                                requirementArrayList.add(materialRequirement);
+
+                                Toast.makeText(thisContext, "Saved requirement", Toast.LENGTH_LONG).show();
+
+                            }
+                    }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            Toast.makeText(thisContext, "Cancelled", Toast.LENGTH_LONG);
+                        }
+                    }).create().show();
+
+                }
+        }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(thisContext, "Cancelled", Toast.LENGTH_LONG);
+            }
+        }).create().show();
 
     }
 }
