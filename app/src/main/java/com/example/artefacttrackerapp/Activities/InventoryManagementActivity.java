cf04ddf41d59833704687ab859ac65e0d897168e
dylan mcadam <dylan@dylanmcadam.co.uk>
2020-04-22 16:10:05 +0100

    initialised a new menu

---
+++ app/src/main/java/com/example/artefacttrackerapp/Activities/InventoryManagementActivity.java
@@ -0,0 +1,21 @@
+package com.example.artefacttrackerapp.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.artefacttrackerapp.R;
+
+public class InventoryManagementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inventory_management);
+        init();
+    }
+
+    private void init(){
+
+    }
+}
