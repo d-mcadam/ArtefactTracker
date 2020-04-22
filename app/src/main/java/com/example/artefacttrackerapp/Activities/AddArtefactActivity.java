c4873fbfd507eb3b9176df00c58f7f2d014e12ec
dylan mcadam <dylan@dylanmcadam.co.uk>
2020-04-22 17:50:28 +0100

    Update AddArtefactActivity.java
    enabling save button if text is entered in the name field

--- app/src/main/java/com/example/artefacttrackerapp/Activities/AddArtefactActivity.java
+++ app/src/main/java/com/example/artefacttrackerapp/Activities/AddArtefactActivity.java
@@ -3,10 +3,17 @@ package com.example.artefacttrackerapp.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.artefacttrackerapp.R;
 
 public class AddArtefactActivity extends AppCompatActivity {
+
+    private Button buttonSave;
+    private EditText inputArtefactName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +24,23 @@ public class AddArtefactActivity extends AppCompatActivity {
 
     private void init(){
 
+        buttonSave = findViewById(R.id.buttonSaveArtefact);
+
+        inputArtefactName = findViewById(R.id.editTextInputArtefactName);
+        inputArtefactName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) { CheckSaveEligibility(); }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) { CheckSaveEligibility(); }
+
+            @Override
+            public void afterTextChanged(Editable editable) { CheckSaveEligibility(); }
+        });
+
+    }
+
+    private void CheckSaveEligibility(){
+        buttonSave.setEnabled(inputArtefactName.getText().toString().trim().length() > 0);
     }
 }
