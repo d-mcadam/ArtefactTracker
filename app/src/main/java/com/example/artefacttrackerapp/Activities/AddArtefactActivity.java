4ffb216466134d33eb2c6c7595d23ee39b5da983
dylan mcadam <dylan@dylanmcadam.co.uk>
2020-04-22 16:36:48 +0100

    Generated the activity and xml file

---
+++ app/src/main/java/com/example/artefacttrackerapp/Activities/AddArtefactActivity.java
@@ -0,0 +1,16 @@
+package com.example.artefacttrackerapp.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.artefacttrackerapp.R;
+
+public class AddArtefactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_artefact);
+    }
+}
