56853ec3b5c08f0b994ac01fa65f732c16d982bd
dylan mcadam <dylan@dylanmcadam.co.uk>
2020-04-22 19:53:50 +0100

    Update MainActivity.java
    passing storage through intent extras

--- app/src/main/java/com/example/artefacttrackerapp/Activities/MainActivity.java
+++ app/src/main/java/com/example/artefacttrackerapp/Activities/MainActivity.java
@@ -6,9 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.artefacttrackerapp.Data.Storage;
 import com.example.artefacttrackerapp.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Storage storage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,15 +22,19 @@ public class MainActivity extends AppCompatActivity {
 
     private void init(){
 
+        storage = new Storage();
+
     }
 
     public void OpenMaterialOptions(View v){
         Intent intent = new Intent(getBaseContext(), MaterialOptionsActivity.class);
+        intent.putExtra(getResources().getResourceName(R.string.intent_key_storage), storage);
         startActivity(intent);
     }
 
     public void OpenInventoryManagement(View v){
         Intent intent = new Intent(getBaseContext(), InventoryManagementActivity.class);
+        intent.putExtra(getResources().getResourceName(R.string.intent_key_storage), storage);
         startActivity(intent);
     }
 }
