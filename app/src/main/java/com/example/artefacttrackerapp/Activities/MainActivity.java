9db061830f838d656e5aba06ab0f72096df25d10
dylan mcadam <dylan@dylanmcadam.co.uk>
2020-04-23 09:27:19 +0100

    Update MainActivity.java
    declaring and referencing main menu view components to set the specified display values - AM NOT CERTAIN STREAM LAMBDAS CURRENTLY WORK, TESTING NOW

--- app/src/main/java/com/example/artefacttrackerapp/Activities/MainActivity.java
+++ app/src/main/java/com/example/artefacttrackerapp/Activities/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.artefacttrackerapp.Data.Storage;
 import com.example.artefacttrackerapp.R;
@@ -12,6 +13,14 @@ import com.example.artefacttrackerapp.R;
 public class MainActivity extends AppCompatActivity {
 
     public static Storage storage;
+
+    private TextView uniqueRemainingCollectionsField;
+    private TextView availableCollectibleField;
+    private TextView availableCronotesField;
+    private TextView availableTetraField;
+    private TextView materialTypeCountField;
+    private TextView ownedArtefactCountField;
+    private TextView requiredMaterialCountField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +33,24 @@ public class MainActivity extends AppCompatActivity {
 
         storage = new Storage();
 
+//        uniqueRemainingCollectionsField = findViewById(R.id.textViewUniqueRemaining);
+//        availableCollectibleField = findViewById(R.id.textViewAvailableCount);
+//        availableCronotesField = findViewById(R.id.textViewCronotesCount);
+//        availableTetraField = findViewById(R.id.textViewTetraCount);
+        materialTypeCountField = findViewById(R.id.textViewMaterialTypeCount);
+        ownedArtefactCountField = findViewById(R.id.textViewArtefactCount);
+        requiredMaterialCountField = findViewById(R.id.textViewMaterialRequirementCount);
+
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+
+        materialTypeCountField.setText(String.valueOf(storage.Materials().size()));
+        ownedArtefactCountField.setText(String.valueOf(storage.Artefacts().stream().map(a -> a.quantity).reduce(0, Integer::sum)));
+
+        storage.Artefacts().stream().forEach(a -> requiredMaterialCountField.setText(a.requirements.stream().map(r -> r.quantity).reduce(0, Integer::sum)));
     }
 
     public void OpenMaterialOptions(View v){
