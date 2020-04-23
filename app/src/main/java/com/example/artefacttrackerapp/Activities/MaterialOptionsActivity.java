bfca83ae03726fc7d8c5abf7fa804b6d46d631ca
dylan mcadam <dylan@dylanmcadam.co.uk>
2020-04-23 11:30:51 +0100

    Update MaterialOptionsActivity.java
    removed unused imports

--- app/src/main/java/com/example/artefacttrackerapp/Activities/MaterialOptionsActivity.java
+++ app/src/main/java/com/example/artefacttrackerapp/Activities/MaterialOptionsActivity.java
@@ -4,14 +4,12 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.example.artefacttrackerapp.Data.Storage;
 import com.example.artefacttrackerapp.R;
 
 import static com.example.artefacttrackerapp.Activities.MainActivity.storage;
