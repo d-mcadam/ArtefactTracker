f952602ee8fd1a0251db729280e03a00ea874ac0
dylan mcadam <dylan@dylanmcadam.co.uk>
2020-04-24 09:25:16 +0100

    declared new menu
    Added declaration to manifest, Initialised the collection activity class and Created collection acitivty layout

---
+++ app/src/main/java/com/example/artefacttrackerapp/Activities/CollectionActivity.java
@@ -0,0 +1,21 @@
+package com.example.artefacttrackerapp.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.artefacttrackerapp.R;
+
+public class CollectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_collection);
+        init();
+    }
+
+    private void init(){
+
+    }
+}
