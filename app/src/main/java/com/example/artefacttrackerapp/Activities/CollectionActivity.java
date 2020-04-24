718d7680e95c0f08059001dc1b3920ad9835ee35
dylan mcadam <dylan@dylanmcadam.co.uk>
2020-04-24 13:24:36 +0100

    Update CollectionActivity.java
    opening new activity

--- app/src/main/java/com/example/artefacttrackerapp/Activities/CollectionActivity.java
+++ app/src/main/java/com/example/artefacttrackerapp/Activities/CollectionActivity.java
@@ -2,6 +2,7 @@ package com.example.artefacttrackerapp.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -21,7 +22,8 @@ public class CollectionActivity extends AppCompatActivity {
     }
 
     public void OpenCollectors(View v){
-
+        Intent intent = new Intent(getBaseContext(), CollectorActivity.class);
+        startActivity(intent);
     }
 
     public void OpenCollections(View v){
