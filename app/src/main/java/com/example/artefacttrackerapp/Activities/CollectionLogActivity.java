8645dc7daf84df55840711f4eec92f911967df1f
dylan mcadam <dylan@dylanmcadam.co.uk>
2020-04-24 16:34:19 +0100

    declared blank activity
    this will be the collection logs activity

---
+++ app/src/main/java/com/example/artefacttrackerapp/Activities/CollectionLogActivity.java
@@ -0,0 +1,23 @@
+package com.example.artefacttrackerapp.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.artefacttrackerapp.R;
+
+public class CollectionLogActivity extends AppCompatActivity {
+
+    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_collection_log);
+        init();
+    }
+
+    private void init(){
+
+    }
+}
