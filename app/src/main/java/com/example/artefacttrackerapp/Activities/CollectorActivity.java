b4976be2d71ff5021b7d1299f4ee09c79fe80a37
dylan mcadam <dylan@dylanmcadam.co.uk>
2020-04-24 13:24:27 +0100

    Update CollectorActivity.java
    initialising filter search feild and category spinner

--- app/src/main/java/com/example/artefacttrackerapp/Activities/CollectorActivity.java
+++ app/src/main/java/com/example/artefacttrackerapp/Activities/CollectorActivity.java
@@ -1,12 +1,32 @@
 package com.example.artefacttrackerapp.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
+import com.example.artefacttrackerapp.Data.Collector;
 import com.example.artefacttrackerapp.R;
 
+import java.util.ArrayList;
+
 public class CollectorActivity extends AppCompatActivity {
+
+    private EditText collectorSearchField;
+    private Spinner categorySpinner;
+
+    private RecyclerView collectorRecyclerView;
+    private RecyclerView.Adapter collectorAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    private final ArrayList<Collector> displayList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +37,43 @@ public class CollectorActivity extends AppCompatActivity {
 
     private void init(){
 
+        //<editor-fold defaultstate="collapsed" desc="Search collectors field">
+        collectorSearchField = findViewById(R.id.editTextSearchCollectors);
+        collectorSearchField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) { RefreshList(); }
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) { RefreshList(); }
+            @Override
+            public void afterTextChanged(Editable editable) { RefreshList(); }
+        });
+        //</editor-fold>
+
+        //<editor-fold defaultstate="collapsed" desc="Category Spinner">
+        ArrayAdapter<CharSequence> categoryAdapter = ArrayAdapter.createFromResource(this, R.array.artefact_categories, android.R.layout.simple_spinner_item);
+        categoryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        categorySpinner = findViewById(R.id.spinnerCollectorArtefactCategory);
+        categorySpinner.setAdapter(categoryAdapter);
+        categorySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) { RefreshList(); }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) { RefreshList(); }
+        });
+        //</editor-fold>
+
+        //<editor-fold defaultstate="collapsed" desc="Recycler view components">
+
+        //</editor-fold>
+
+        RefreshList();
+
     }
+
+    public void RefreshList(){
+
+        displayList.clear();
+
+    }
+
 }
