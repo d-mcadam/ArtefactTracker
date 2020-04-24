7cca7d1412f1167c5d8d781c56b84de706271a7b
dylan mcadam <dylan@dylanmcadam.co.uk>
2020-04-24 13:45:45 +0100

    Update CollectorActivity.java
    generating a dialog to createa  collector object

--- app/src/main/java/com/example/artefacttrackerapp/Activities/CollectorActivity.java
+++ app/src/main/java/com/example/artefacttrackerapp/Activities/CollectorActivity.java
@@ -1,21 +1,27 @@
 package com.example.artefacttrackerapp.Activities;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.artefacttrackerapp.Data.Collector;
 import com.example.artefacttrackerapp.R;
 
 import java.util.ArrayList;
+
+import static com.example.artefacttrackerapp.Activities.MainActivity.storage;
 
 public class CollectorActivity extends AppCompatActivity {
 
@@ -73,6 +79,35 @@ public class CollectorActivity extends AppCompatActivity {
     public void RefreshList(){
 
         displayList.clear();
+
+    }
+
+    public void AddCollector(View v){
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Create a Collector");
+
+        LayoutInflater inflater = getLayoutInflater();
+
+        View dialogView = inflater.inflate(R.layout.dialog_create_collector, null);
+        final EditText inputFieldName = dialogView.findViewById(R.id.editTextCreateCollectorName);
+        final EditText inputFieldLocation = dialogView.findViewById(R.id.editTextCreateCollectorLocation);
+
+        dialog.setView(dialogView)
+            .setPositiveButton("Add", (dialogInterface, i) -> {
+
+            final String inputName = inputFieldName.getText().toString().trim();
+            final String inputLocation = inputFieldLocation.getText().toString().trim();
+
+            if (storage.Collectors().stream().anyMatch(c -> c.name.equals(inputName))){
+                Toast.makeText(getBaseContext(), "Duplicate names detected", Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            storage.AddCollector(new Collector(inputName, inputLocation));
+            Toast.makeText(getBaseContext(), "Added collector: " + inputName + " @ " + inputLocation, Toast.LENGTH_LONG).show();
+            collectorSearchField.setText("");
+
+        }).setNegativeButton("Cancel", (dialogInterface, i) -> Toast.makeText(getBaseContext(), "Cancelled", Toast.LENGTH_LONG).show()).create().show();
 
     }
 
