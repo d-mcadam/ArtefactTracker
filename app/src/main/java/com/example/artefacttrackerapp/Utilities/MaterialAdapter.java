017c18b372948f624cd74dc7b7d4cc1860b6ec0e
dylan mcadam <dylan@dylanmcadam.co.uk>
2020-04-25 20:00:20 +0100

    Update MaterialAdapter.java
    increasing and decreasing material quantities

--- app/src/main/java/com/example/artefacttrackerapp/Utilities/MaterialAdapter.java
+++ app/src/main/java/com/example/artefacttrackerapp/Utilities/MaterialAdapter.java
@@ -1,5 +1,6 @@
 package com.example.artefacttrackerapp.utilities;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.graphics.Color;
 import android.view.LayoutInflater;
@@ -20,6 +21,30 @@ import java.util.ArrayList;
 import static com.example.artefacttrackerapp.activities.MainActivity.storage;
 
 public class MaterialAdapter extends RecyclerView.Adapter<MaterialAdapter.MaterialViewHolder> {
+
+    private void incrementQuantity(Material material){
+        material.quantity++;
+        notifyDataSetChanged();
+    }
+    private void decrementQuantity(Material material){
+        if (material.quantity < 1){
+
+            AlertDialog.Builder dialog = new AlertDialog.Builder(context);
+            dialog.setTitle("Warning");
+            dialog.setMessage("You are about to delete the Material entry entirely");
+
+            dialog.setPositiveButton("Continue", (dialogInterface, i) -> {
+
+                storage.DeleteMaterial(material);
+                ((MaterialOptionsActivity)context).RefreshList();
+
+            }).setNegativeButton("Cancel", null).create().show();
+
+        }else{
+            material.quantity--;
+            notifyDataSetChanged();
+        }
+    }
 
     private final Context context;
     private final ArrayList<Material> materialDataSet;
@@ -56,19 +81,21 @@ public class MaterialAdapter extends RecyclerView.Adapter<MaterialAdapter.Materi
             notifyDataSetChanged();
         });
 
-        holder.deleteButton.setOnClickListener(view -> {
-            this.selectedPosition = -1;
-            storage.DeleteMaterial(material);
-            ((MaterialOptionsActivity)context).RefreshList();
-        });
+        holder.itemView.setOnLongClickListener(view -> ((MaterialOptionsActivity)context).GenerateLocationInputDialog(material));
+
+        holder.plusButton.setOnClickListener(view -> incrementQuantity(material));
+
+        holder.minusButton.setOnClickListener(view -> decrementQuantity(material));
 
         holder.viewIsSelected = selectedPosition == thisViewsPosition;
 
         holder.itemView.setBackgroundColor(holder.viewIsSelected ? context.getResources().getColor(R.color.colourRecyclerViewSelected, null) : Color.TRANSPARENT);
         holder.detailView.setText(context.getString(R.string.place_holder_title, material.title));
         holder.qtyView.setText(context.getString(R.string.place_holder_quantity, material.quantity));
-        holder.deleteButton.setVisibility(holder.viewIsSelected ? View.VISIBLE : View.INVISIBLE);
-        holder.deleteButton.setClickable(holder.viewIsSelected);
+        holder.plusButton.setVisibility(holder.viewIsSelected ? View.VISIBLE : View.INVISIBLE);
+        holder.plusButton.setClickable(holder.viewIsSelected);
+        holder.minusButton.setVisibility(holder.viewIsSelected ? View.VISIBLE : View.INVISIBLE);
+        holder.minusButton.setClickable(holder.viewIsSelected);
 
     }
 
@@ -81,15 +108,22 @@ public class MaterialAdapter extends RecyclerView.Adapter<MaterialAdapter.Materi
 
         private final TextView detailView;
         private final TextView qtyView;
-        private final ImageButton deleteButton;
+        private final ImageButton plusButton;
+        private final ImageButton minusButton;
 
         private MaterialViewHolder(@NonNull View itemView) {
             super(itemView);
             detailView = itemView.findViewById(R.id.textViewHolderMaterial);
             detailView.setHeight(viewHolderHeight);
+
             qtyView = itemView.findViewById(R.id.textViewHolderMaterialDisplayQuantity);
             qtyView.setHeight(viewHolderHeight);
-            deleteButton = itemView.findViewById(R.id.imageButtonHolderDeleteMaterial);
+
+            plusButton = itemView.findViewById(R.id.imageButtonHolderPlusMaterial);
+            plusButton.getLayoutParams().height = viewHolderHeight;
+
+            minusButton = itemView.findViewById(R.id.imageButtonHolderMinusMaterial);
+            minusButton.getLayoutParams().height = viewHolderHeight;
         }
 
     }
