f0936498468d1298cbaeb9efa7c9b7396ad536d8
dylan mcadam <dylan@dylanmcadam.co.uk>
2020-04-25 09:17:39 +0100

    Update CollectionLogActivity.java
    made code more readable and got the filter artefact field working when selecting artefacts to add to a collection

--- app/src/main/java/com/example/artefacttrackerapp/Activities/CollectionLogActivity.java
+++ app/src/main/java/com/example/artefacttrackerapp/Activities/CollectionLogActivity.java
@@ -18,7 +18,7 @@ import android.widget.Toast;
 
 import com.example.artefacttrackerapp.R;
 import com.example.artefacttrackerapp.data.Collection;
-import com.example.artefacttrackerapp.data.Collector;
+import com.example.artefacttrackerapp.data.GameArtefact;
 import com.example.artefacttrackerapp.utilities.CollectionAdapter;
 import com.example.artefacttrackerapp.utilities.SelectArtefactAdapter;
 
@@ -117,13 +117,16 @@ public class CollectionLogActivity extends AppCompatActivity {
 
     public void AddCollectionLog(View v){
 
+        //<editor-fold defaultstate="collapsed" desc="this context variable and check collector count">
         final Context thisContext = this;
 
         if (storage.Collectors().size() < 1){
             Toast.makeText(this, "No collectors in storage", Toast.LENGTH_LONG).show();
             return;
         }
+        //</editor-fold>
 
+        //<editor-fold defaultstate="collapsed" desc="Declare dialog and view and reference activity components">
         AlertDialog.Builder collectionDialog = new AlertDialog.Builder(this);
         collectionDialog.setTitle("Create a Collection");
 
@@ -133,15 +136,19 @@ public class CollectionLogActivity extends AppCompatActivity {
         final Spinner inputCategorySpinner = collectionDialogView.findViewById(R.id.spinnerHolderCreateCollectionArtefactCategory);
         final Spinner inputRewardSpinner = collectionDialogView.findViewById(R.id.spinnerHolderCreateCollectionReward);
         final EditText inputQtyField = collectionDialogView.findViewById(R.id.editTextHolderCreateCollectionRewardQuantity);
+        //</editor-fold>
 
         inputNameField.setText(collectionSearchField.getText().toString().trim());
 
+        //<editor-fold defaultstate="collapsed" desc="Populate collector list">
         ArrayList<String> collectorValues = new ArrayList<>();
         storage.Collectors().forEach(c -> collectorValues.add(c.name));
         ArrayAdapter<String> collectorAdapter = new ArrayAdapter<>(thisContext, R.layout.support_simple_spinner_dropdown_item, collectorValues);
         collectorAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
         inputCollectorSpinner.setAdapter(collectorAdapter);
+        //</editor-fold>
 
+        //<editor-fold defaultstate="collapsed" desc="Populate category and reward spinners from resource">
         ArrayAdapter<CharSequence> categoryAdapter = ArrayAdapter.createFromResource(this, R.array.artefact_categories, android.R.layout.simple_spinner_item);
         categoryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         inputCategorySpinner.setAdapter(categoryAdapter);
@@ -149,10 +156,12 @@ public class CollectionLogActivity extends AppCompatActivity {
         ArrayAdapter<CharSequence> rewardAdapter = ArrayAdapter.createFromResource(this, R.array.collection_rewards, android.R.layout.simple_spinner_item);
         rewardAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         inputRewardSpinner.setAdapter(rewardAdapter);
+        //</editor-fold>
 
         collectionDialog.setView(collectionDialogView)
             .setPositiveButton("Add", (dialogInterface, i) -> {
 
+                //<editor-fold defaultstate-"collapsed" desc="Check details filled in & get data">
                 if (inputNameField.getText().toString().trim().equals("") || inputQtyField.getText().toString().trim().equals("")){
                     Toast.makeText(thisContext, "Fill in all the fields", Toast.LENGTH_LONG).show();
                     return;
@@ -163,20 +172,52 @@ public class CollectionLogActivity extends AppCompatActivity {
                 final String inputCategory = inputCategorySpinner.getSelectedItem().toString();
                 final String inputRewardType = inputRewardSpinner.getSelectedItem().toString();
                 final int inputRewardQty = Integer.parseInt(inputQtyField.getText().toString().trim());
+                //</editor-fold>
 
+                //<editor-fold defaultstate="collapsed" desc="Declare collection object, dialog and view">
                 Collection collection = new Collection(inputName, inputCollector, inputCategory, inputRewardType, inputRewardQty);
 
                 AlertDialog.Builder listDialog = new AlertDialog.Builder(thisContext);
                 listDialog.setTitle("Select the Artefacts");
 
                 View listDialogView = getLayoutInflater().inflate(R.layout.dialog_collection_list, null);
-                final EditText inputSearchField = listDialogView.findViewById(R.id.editTextHolderCollectionListSearch);
+                //</editor-fold>
+
+                //==========================================================
+                //DECLARATION OF THESE EVENTS AND VARIABLES MUST BE IN ORDER
+                //==========================================================
+
+                //<editor-fold defaultstate="collapsed" desc="Declare recycler view components">
+                final ArrayList<GameArtefact> dialogDisplayList = new ArrayList<>();
 
                 final RecyclerView inputRecyclerView = listDialogView.findViewById(R.id.recyclerViewHolderCollectionLogList);
                 inputRecyclerView.setLayoutManager(new LinearLayoutManager(thisContext));
 
-                RecyclerView.Adapter inputRecyclerViewAdapter = new SelectArtefactAdapter(thisContext, storage.Artefacts());
+                RecyclerView.Adapter inputRecyclerViewAdapter = new SelectArtefactAdapter(thisContext, dialogDisplayList);
                 inputRecyclerView.setAdapter(inputRecyclerViewAdapter);
+                //</editor-fold>
+
+                //<editor-fold defaultstate="collapsed" desc="Declare search field and function">
+                final EditText inputSearchField = listDialogView.findViewById(R.id.editTextHolderCollectionListSearch);
+                inputSearchField.addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) { RefreshDialogList(); }
+                    @Override
+                    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) { RefreshDialogList(); }
+                    @Override
+                    public void afterTextChanged(Editable editable) { RefreshDialogList(); }
+                    private void RefreshDialogList(){
+                        dialogDisplayList.clear();
+                        String searchText = inputSearchField.getText().toString().trim();
+                        storage.Artefacts().stream().filter(a ->
+                                searchText.length() < 1 || a.title.contains(searchText)
+                        ).forEach(dialogDisplayList::add);
+                        ((SelectArtefactAdapter)inputRecyclerViewAdapter).selectedPosition = -1;
+                        inputRecyclerViewAdapter.notifyDataSetChanged();
+                    }
+                });
+                inputSearchField.setText("");
+                //</editor-fold>
 
                 listDialog.setView(listDialogView)
                     .setPositiveButton("Save", (dialogInterface1, i1) -> {
