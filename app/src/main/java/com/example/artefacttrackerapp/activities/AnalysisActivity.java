535602d6c0d334e43de5730daf2d1d576f96441c
dylan mcadam <dylan@dylanmcadam.co.uk>
2020-05-03 19:31:52 +0100

    working adapter

--- app/src/main/java/com/example/artefacttrackerapp/activities/AnalysisActivity.java
+++ app/src/main/java/com/example/artefacttrackerapp/activities/AnalysisActivity.java
@@ -1,12 +1,29 @@
 package com.example.artefacttrackerapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 
 import com.example.artefacttrackerapp.R;
+import com.example.artefacttrackerapp.data.GameArtefact;
+import com.example.artefacttrackerapp.utilities.GenReqArtefactAdapter;
+
+import java.util.ArrayList;
+
+import static com.example.artefacttrackerapp.activities.MainActivity.storage;
 
 public class AnalysisActivity extends AppCompatActivity {
+
+    private EditText searchField;
+
+    private RecyclerView.Adapter adapter;
+
+    private final ArrayList<GameArtefact> displayList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +33,39 @@ public class AnalysisActivity extends AppCompatActivity {
     }
 
     private void init(){
+
+        searchField = findViewById(R.id.editTextRequirementGenArtefactFilter);
+        searchField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) { RefreshList(); }
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) { RefreshList(); }
+            @Override
+            public void afterTextChanged(Editable editable) { RefreshList(); }
+        });
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerViewGenReqArtefacts);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        adapter = new GenReqArtefactAdapter(this, displayList);
+        recyclerView.setAdapter(adapter);
+
+        RefreshList();
+    }
+
+    public void RefreshList(){
+
+        displayList.clear();
+
+        String textSearch = searchField.getText().toString().trim();
+
+        storage.Artefacts().stream().filter(a ->
+                textSearch.length() < 1 || a.title.toLowerCase().contains(textSearch.toLowerCase())
+        ).forEach(displayList::add);
+
+        ((GenReqArtefactAdapter)adapter).selectedPosition = -1;
+        adapter.notifyDataSetChanged();
 
     }
 }
---
+++ app/src/main/java/com/example/artefacttrackerapp/utilities/GenReqArtefactAdapter.java
@@ -0,0 +1,110 @@
+package com.example.artefacttrackerapp.utilities;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.artefacttrackerapp.R;
+import com.example.artefacttrackerapp.data.GameArtefact;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.stream.Collectors;
+
+public class GenReqArtefactAdapter extends RecyclerView.Adapter<GenReqArtefactAdapter.GenReqViewHolder> {
+
+    private final Context context;
+    private final ArrayList<GameArtefact> displayList;
+    private final ArrayList<GameArtefact> selectedList = new ArrayList<>();
+
+    public int selectedPosition = -1;
+
+    private final int viewHolderHeight = 129;
+
+    public GenReqArtefactAdapter(Context context, ArrayList<GameArtefact> dataSet){
+        this.context = context;
+        this.displayList = dataSet;
+    }
+
+    @NonNull
+    @Override
+    public GenReqViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.view_holder_artefact, parent, false);
+        v.getLayoutParams().height = viewHolderHeight;
+        return new GenReqViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull GenReqViewHolder holder, int thisViewsPosition) {
+
+        final GameArtefact artefact = displayList.get(thisViewsPosition);
+
+        holder.itemView.setOnClickListener(view -> {
+            notifyDataSetChanged();
+
+            if (holder.viewIsSelected)
+                selectedPosition = -1;
+            else
+                selectedPosition = thisViewsPosition;
+
+            notifyDataSetChanged();
+        });
+
+        holder.viewIsSelected = selectedPosition == thisViewsPosition;
+
+        holder.itemView.setBackgroundColor(holder.viewIsSelected ? context.getColor(R.color.colour_recycler_view_selected_grey) : Color.TRANSPARENT);
+
+        holder.plusButton.setOnClickListener(view -> {
+            selectedList.add(artefact);
+            notifyDataSetChanged();
+        });
+
+        holder.minusButton.setOnClickListener(view -> {
+            selectedList.remove(artefact);
+            notifyDataSetChanged();
+        });
+
+        holder.detailView.setText(context.getString(R.string.place_holder_title, artefact.title));
+        holder.qtyView.setText(String.valueOf(Collections.frequency(selectedList, artefact)));
+
+        holder.plusButton.setVisibility(holder.viewIsSelected & Collections.frequency(selectedList, artefact) < artefact.quantity ? View.VISIBLE : View.INVISIBLE);
+        holder.plusButton.setClickable(holder.viewIsSelected & Collections.frequency(selectedList, artefact) < artefact.quantity);
+        holder.minusButton.setVisibility(holder.viewIsSelected & selectedList.contains(artefact) ? View.VISIBLE : View.INVISIBLE);
+        holder.minusButton.setClickable(holder.viewIsSelected & selectedList.contains(artefact));
+    }
+
+    @Override
+    public int getItemCount() { return this.displayList.size(); }
+
+    public class GenReqViewHolder extends RecyclerView.ViewHolder {
+
+        private boolean viewIsSelected = false;
+
+        private TextView detailView;
+        private TextView qtyView;
+        private ImageButton plusButton;
+        private ImageButton minusButton;
+
+        public GenReqViewHolder(@NonNull View itemView) {
+            super(itemView);
+            detailView = itemView.findViewById(R.id.textViewHolderArtefactName);
+            detailView.setHeight(viewHolderHeight);
+
+            qtyView = itemView.findViewById(R.id.textViewHolderArtefactQuantity);
+            qtyView.setHeight(viewHolderHeight);
+
+            plusButton = itemView.findViewById(R.id.imageButtonHolderAddArtefact);
+            plusButton.getLayoutParams().height = viewHolderHeight;
+
+            minusButton = itemView.findViewById(R.id.imageButtonHolderMinusArtefact);
+            minusButton.getLayoutParams().height = viewHolderHeight;
+        }
+    }
+}
